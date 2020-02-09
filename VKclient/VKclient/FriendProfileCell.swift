7906dd76c9ecce9b2fab1a2e818157806870e15e
Buinovskiy Stanislav <sbuinovsky@gmail.com>
2020-02-10 00:50:22 +0300

    Сделано отображение списка друзей и профиля друга

---
+++ VKclient/VKclient/FriendProfileCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendProfileCell.swift
+//  VKclient
+//
+//  Created by Станислав Буйновский on 09.02.2020.
+//  Copyright © 2020 Станислав Буйновский. All rights reserved.
+//
+
+import UIKit
+
+class FriendProfileCell: UICollectionViewCell {
+    @IBOutlet weak var FriendProfileImage: UIImageView!
+    
+}
