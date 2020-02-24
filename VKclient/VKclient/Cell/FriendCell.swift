e7907f9575f80acf4053281fc8148b3804c787b4
Buinovskiy Stanislav <sbuinovsky@gmail.com>
2020-02-24 22:32:28 +0300

    Сделал п. 2 и 3 из ДЗ 7
    - добавлена анимация лайка и его счетчика на экране ленты новостей
    - добавлена анимация аватарки пользователя на экране списка друзей

--- VKclient/VKclient/Cell/FriendCell.swift
+++ VKclient/VKclient/Cell/FriendCell.swift
@@ -18,4 +18,34 @@ class FriendCell: UITableViewCell {
         friendNameLabel.text = ""
     }
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //добавляем обработку нажатия на элемент
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTap(_:)))
+        
+        friendAvatarImage.addGestureRecognizer(tapGesture)
+        friendAvatarImage.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func onTap(_ sender: UIGestureRecognizer) {
+        print(#function)
+        
+        
+        UIView.animate(withDuration: 0.2,
+                       delay: 0,
+                       animations: {
+                        self.friendAvatarImage.transform = .init(scaleX: 0.9, y: 0.9)
+        })
+        
+        UIView.animate(withDuration: 1,
+                       delay: 0.2,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 30,
+                       animations: {
+                        self.friendAvatarImage.transform = .init(scaleX: 1, y: 1)
+        })
+    }
+    
 }
