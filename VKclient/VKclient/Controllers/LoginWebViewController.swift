cea7bc1f6255a7e29367c53619b7f6b1c03bab33
Buinovskiy Stanislav <sbuinovsky@gmail.com>
2020-04-02 23:17:41 +0300

    added "wall" to scopes when login

--- VKclient/VKclient/Controllers/LoginWebViewController.swift
+++ VKclient/VKclient/Controllers/LoginWebViewController.swift
@@ -30,7 +30,7 @@ class LoginWebViewController: UIViewController {
         URLQueryItem(name: "client_id", value: "7359889"),
         URLQueryItem(name: "display", value: "mobile"),
         URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-        URLQueryItem(name: "scope", value: "users,friends,groups,photos"),
+        URLQueryItem(name: "scope", value: "users,friends,groups,photos,wall"),
         URLQueryItem(name: "response_type", value: "token"),
         URLQueryItem(name: "v", value: "5.103"),
         ]
