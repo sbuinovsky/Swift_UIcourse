72da9af872060863815f73e5210c0ba0878fd3e6
Buinovsky Stanislav <sbuinovsky@gmail.com>
2020-04-24 23:03:55 +0300

    NewsSource model

---
+++ VKclient/VKclient/Model/NewsSource.swift
@@ -0,0 +1,9 @@
+//
+//  NewsSource.swift
+//  VKclient
+//
+//  Created by Станислав Буйновский on 24.04.2020.
+//  Copyright © 2020 Станислав Буйновский. All rights reserved.
+//
+
+import Foundation
