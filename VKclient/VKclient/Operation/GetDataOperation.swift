9c5615e2acffdf9aadd2c7573a559e207e799c34
Buinovsky Stanislav <sbuinovsky@gmail.com>
2020-04-28 11:15:40 +0300

    Add GetDataOperation

--- VKclient/VKclient/Operation/GetDataOperation.swift
+++ VKclient/VKclient/Operation/GetDataOperation.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Станислав Буйновский. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+class GetDataOperation: AsyncOperation {
+    
+    private var request: DataRequest
+    var data: Data?
+    
+    override func main() {
+        request.responseData(queue: DispatchQueue.global()) { [weak self] response in
+            self?.data = response.data
+            self?.state = .finished
+            
+        }
+    }
+    
+    init(request: DataRequest) {
+        self.request = request
+    }
+}
