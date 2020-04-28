4cf4d9c4555d8348866e06d4f03a0833ea3c658b
Buinovsky Stanislav <sbuinovsky@gmail.com>
2020-04-28 11:15:23 +0300

    Add AsyncOperation

---
+++ VKclient/VKclient/Operation/AsyncOperation.swift
@@ -0,0 +1,60 @@
+//
+//  AsyncOperation.swift
+//  VKclient
+//
+//  Created by Станислав Буйновский on 28.04.2020.
+//  Copyright © 2020 Станислав Буйновский. All rights reserved.
+//
+
+import UIKit
+
+class AsyncOperation: Operation {
+    
+    enum State: String {
+        case ready, executing, finished
+        fileprivate var keyPath: String {
+            return "is" + rawValue.capitalized
+        }
+    }
+    
+    var state = State.ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
+        }
+        didSet {
+            didChangeValue(forKey: state.keyPath)
+            didChangeValue(forKey: oldValue.keyPath)
+        }
+    }
+    
+    override var isAsynchronous: Bool {
+        return true
+    }
+    
+    override var isReady: Bool {
+        return super.isReady && state == .ready
+    }
+    
+    override var isExecuting: Bool {
+        return state == .executing
+    }
+    
+    override var isFinished: Bool {
+        return state == .finished
+    }
+    
+    override func start() {
+        if isCancelled {
+            state = .finished
+        } else {
+            main()
+            state = .executing
+        }
+    }
+    
+    override func cancel() {
+        super.cancel()
+        state = .finished
+    }
+}
---
+++ VKclient/VKclient/Operation/ParseGroupsOperation.swift
@@ -0,0 +1,9 @@
+//
+//  ParseGroupsOperation.swift
+//  VKclient
+//
+//  Created by Станислав Буйновский on 28.04.2020.
+//  Copyright © 2020 Станислав Буйновский. All rights reserved.
+//
+
+import Foundation
