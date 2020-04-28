6893504a3a01764271fd3273c2b166a0956fece3
Buinovsky Stanislav <sbuinovsky@gmail.com>
2020-04-28 11:16:02 +0300

    Add ParseGroupsOperation

--- VKclient/VKclient/Operation/ParseGroupsOperation.swift
+++ VKclient/VKclient/Operation/ParseGroupsOperation.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Станислав Буйновский. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftyJSON
+
+class ParseGroupsOperation: Operation {
+    
+    var outputData: [Group] = []
+    
+    override func main() {
+        guard let getDataOperation = dependencies.first as? GetDataOperation,
+            let data = getDataOperation.data else { return }
+        
+        do {
+            let json = try JSON(data: data)
+            let groups: [Group] = json.map { item -> Group in
+                let group: Group = .init()
+                group.id = item.1["id"].intValue
+                group.name = item.1["name"].stringValue
+                group.avatar = item.1["photo_200"].stringValue
+                return group
+            }
+            outputData = groups
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
