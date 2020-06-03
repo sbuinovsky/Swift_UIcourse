42a379f02cdd6c739cd7a26dd988c92cdc267cc2
Buinovskiy Stanislav <sbuinovsky@gmail.com>
2020-06-03 11:58:10 +0300

    Realm install

---
+++ Millioner/Millioner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millioner
+//
+//  Created by Станислав Буйновский on 02.06.2020.
+//  Copyright © 2020 Станислав Буйновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
