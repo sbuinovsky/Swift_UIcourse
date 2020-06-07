7c32a440ee55154b1e66c65f29f8c67fcfb3c337
Buinovskiy Stanislav <sbuinovsky@gmail.com>
2020-06-07 21:49:39 +0300

    Add Settings VC

---
+++ Millioner/Millioner/Controller/SettingsVC.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsVC.swift
+//  Millioner
+//
+//  Created by Станислав Буйновский on 07.06.2020.
+//  Copyright © 2020 Станислав Буйновский. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    @IBOutlet weak var difficulty: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+}
